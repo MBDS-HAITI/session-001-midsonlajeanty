69e1841e8d61ef29fabfaccc53f67a5de143d25a
Louis Midson LAJEANTY <louismidsonlajeanty@gmail.com>
2025-12-04 06:03:41 -0500

    chore: build app for deployment

--- session-01/src/components/pages/About.jsx
+++ session-01/src/components/pages/About.jsx
@@ -7,6 +7,17 @@ export default function About() {
         enseignants et les étudiants à suivre les notes, les matières et les
         informations des étudiants.
       </p>
+
+      <p>
+        Ce projet est réalisé par Louis Midson LAJEANTY, étudiant au programe de
+        master MBDS de la Faculté des Sciences et de l'Université Côte d'Azur
+      </p>
+
+      <ul>
+        <li>Louis Midson LAJEANTY</li>
+        <li>louis_midson.lajeanty@student.ueh.edu.ht</li>
+        <li>+509 46 95 4516 / 40 21 7596</li>
+      </ul>
     </div>
   );
 }
