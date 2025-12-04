5ed4e82132f1dce019d4ca0dfbb0a760ff31ee3e
Louis Midson LAJEANTY <louismidsonlajeanty@gmail.com>
2025-12-03 23:54:43 -0500

    TD004: Add menu

--- session-01/src/components/Header.jsx
+++ session-01/src/components/Header.jsx
@@ -1,11 +1,30 @@
-import logo from '../assets/mbds.png';
+import "./Header.css";
+import logo from "../assets/mbds.png";
 
 export default function Header() {
+  function handleClick(item) {
+    alert(item);
+  }
+
   return (
     <header>
+      <a href="/">
         <img src={logo} alt="Logo MBDS" />
-        <h1>Introduction à React</h1>
-        <p>A la découverte des premières notions de React</p>
+      </a>
+      <nav>
+        <a href="#" onClick={() => handleClick("Notes")}>
+          Notes
+        </a>
+        <a href="#" onClick={() => handleClick("Etudiants")}>
+          Etudiants
+        </a>
+        <a href="#" onClick={() => handleClick("Matières")}>
+          Matières
+        </a>
+        <a href="#" onClick={() => handleClick("A propos")}>
+          A propos
+        </a>
+      </nav>
     </header>
-  )
+  );
 }
