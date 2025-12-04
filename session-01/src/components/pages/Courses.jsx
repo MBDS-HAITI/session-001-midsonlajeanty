13a539dd432c26542556588c0e440281ffedba7f
Louis Midson LAJEANTY <louismidsonlajeanty@gmail.com>
2025-12-04 05:46:25 -0500

    TD05: Remplir le contenu des composants

--- session-01/src/components/pages/Courses.jsx
+++ session-01/src/components/pages/Courses.jsx
@@ -1,3 +1,12 @@
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+
 export default function Courses({ data }) {
   const cousesSet = new Set(data.map((item) => item.course));
   const uniqueCourses = Array.from(cousesSet);
@@ -5,13 +14,27 @@ export default function Courses({ data }) {
   return (
     <div>
       <h2>Matières</h2>
-      <ul>
-        {uniqueCourses.map((item) => (
-          <li key={item}>
-            <h3>{item}</h3>
-          </li>
-        ))}
-      </ul>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Matière</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {uniqueCourses.map((course) => (
+              <TableRow
+                key={course}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row" align="center">
+                  {course}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
--- session-01/src/components/pages/Students.jsx
+++ session-01/src/components/pages/Students.jsx
@@ -1,15 +1,40 @@
-export default function Students({ data }) {
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
+export default function Students({ data }) {
   return (
     <div>
       <h2>Étudiants</h2>
-      <ul>
-        {data.map((item) => (
-          <li key={item.student.firstname + item.student.lastname}>
-            <h3>{item.student.firstname} {item.student.lastname}</h3>
-          </li>
-        ))}
-      </ul>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="center">Prénom</TableCell>
+              <TableCell align="center">Nom</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((row) => (
+              <TableRow
+                key={row.student.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.student.id}
+                </TableCell>
+                <TableCell align="center">{row.student.firstname}</TableCell>
+                <TableCell align="center">{row.student.lastname}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
